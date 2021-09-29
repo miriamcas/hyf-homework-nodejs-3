98fee040bd3b42ef5a0802f0ead04bae0a790930
miriamcas <miriam@graywolf.co>
2021-09-28 19:54:59 -0500

    users commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/',function(req,res){
-	res.send("Hello World!")
+app.get('/',function(req, res){
+	res.send('Hello World!');
+ });
+ let users= [];
+ /*users.push('Miriam');
+ users.push('Lupita');
+ users.push('Giovanni');*/
+
+app.get('/users',function(req,res){	
+    res.send(users);
 })
 
 app.listen(3000,function(){console.log('server is listening')})
